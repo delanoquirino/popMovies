8b35f197ea301fe409427e88382c4646cb4fc847
delanoquirino <delanoquirino@gmail.com>
2023-01-25 21:30:48 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { NavBar } from "./components/NavBar/index";
 import { GlobalStyle } from "./styles/globals";
 
 function App() {
   return (
     <>
+      <NavBar />
       <GlobalStyle />
-
       <Outlet />
     </>
   );
